791fbecce12b4dd80c8712b1753721333d9621d8
Abdel Rahman Mansour <65471490+Graycouch@users.noreply.github.com>
2022-08-21 14:57:14 -0500

    Fixed up the data

--- src/components/ExperienceData.js
+++ src/components/ExperienceData.js
@@ -1,20 +1,20 @@
 export const Work = [
     {
         id:1,
-        name:"JPMorgan Chase & Co. SWE Intern",
+        name:" Software Engineering Intern",
         description:"Created a Full-Stack Web Application from scratch that allows conflicts officers to view different types of conflicts of interests from across the firm and deal with them accordingly",
-        tags:["From: June 2022 - August 2022"],
+        tags:["Company: JPMorgan Chase & Co.", "Location: Houston, TX", "From: June 2022 - August 2022"],
     },
     {
         id:2,
         name:"EXCELD Tutor",
         description:"Conducted rigorous instruction of fundamental curriculum while identifying subjects that students struggle with to improve GPA by an average of two letter grades",
-        tags:["From: Dec 2020 - Dec 2021"],
+        tags:["Company: Louisiana State University", "Location: Baton Rouge, LA", "From: Dec 2020 - May 2022"],
     },
     {
         id:3,
         name:"Research Scholar",
         description:"Worked in Dr. Chen Wang’s Mobile and Internet Security Lab on a Gesture Controlled Robotic Hand Project, while also assisting fellow researchers with their projects",
-        tags:["From: Dec 2020 - May 2021"],
+        tags:["Company: Louisiana State University", "Location: Baton Rouge, LA", "From: Dec 2020 - May 2021"],
     },
 ] 
