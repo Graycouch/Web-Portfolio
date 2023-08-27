8b0f43509fcb546b990affacf3a9c7144cb5a320
Graycouch <abdelrahmanmansour@outlook.com>
2023-08-27 12:56:00 -0500

    Updated experience data

--- src/components/ExperienceData.js
+++ src/components/ExperienceData.js
@@ -1,24 +1,30 @@
 export const Work = [
     {
         id:1,
-        name:" Software Engineering Intern",
-        description:"Creating an AutoPay feature that tracks the duration a user is parked and allows the user to automatically pay for a parking spot once they leave",
-        tags:["Company: ParkZen", "Location: Baton Rouge, LA", "From: Oct 2022 - Present"],
+        name:" Software Engineer",
+        description:"Currently undergoing extensive training to improve my skill set",
+        tags:["Company: IBM", "Location: Baton Rouge, LA", "From: July 2023 - Present"],
     },
     {
         id:2,
+        name:" Software Engineering Intern",
+        description:"Created an AutoPay feature that tracks the duration a user is parked and allows the user to automatically pay for a parking spot once they leave",
+        tags:["Company: ParkZen", "Location: Baton Rouge, LA", "From: Oct 2022 - June 2023"],
+    },
+    {
+        id:3,
         name:" Software Engineering Intern",
         description:"Created a Full-Stack Web Application from scratch that allows conflicts officers to view different types of conflicts of interests from across the firm and deal with them accordingly",
         tags:["Company: JPMorgan Chase", "Location: Houston, TX", "From: June 2022 - Aug 2022"],
     },
     {
-        id:3,
+        id:4,
         name:"EXCELD Tutor",
         description:"Provided rigorous instruction of fundamental curriculum while identifying key topics that students struggle with in Computer Science and Calculus courses",
         tags:["Company: LSU", "Location: Baton Rouge, LA", "From: Dec 2020 - May 2022"],
     },
     {
-        id:4,
+        id:5,
         name:"Research Scholar",
         description:"Conducted research in Dr. Chen Wang's Mobile and Internet Security Lab on a Gesture Controlled Robotic Arm project, while also assisting fellow researchers with their projects",
         tags:["Company: LSU", "Location: Baton Rouge, LA", "From: Dec 2020 - May 2021"],
