d0d690c22d69c8fff2a49dc06ee9a4ab0ce05c9f
Abdel Rahman Mansour <abdelrahmanmansour@outlook.com>
2025-10-21 23:59:50 -0500

    Updated data

--- src/components/ProjectsData.js
+++ src/components/ProjectsData.js
@@ -2,36 +2,36 @@ export const Work = [
     {
         id:1,
         name:"Tutor Vision",
-        description:"Full-Stack Web Application that mimics many of the features that Facebook provides such as allowing you to make an account, follow other people, post images, and message your friends",
-        tags:["React Native","Javascript","MongoDB", "AWS", "Node.js", "Express.js"],
+        description:"Designed and developed a full-stack mobile application that enables university students to locate nearby tutors based on subject and availability using the MERN stack (MongoDB, Express.js, React Native, Node.js).",
+        tags:["Amazon Web Services (AWS)","React Native","Node.js"],
         
         demo:"https://github.com/Graycouch/Tutor-Vision",
         github:"https://github.com/Graycouch/Tutor-Vision"
     },
     {
         id:2,
-        name:"GameHub",
-        description:"Full-Stack Web Application that mimics many of the features that Facebook provides such as allowing you to make an account, follow other people, post images, and message your friends",
-        tags:["React","Javascript","MongoDB", "Socket.io", "Node.js", "Express.js"],
-        
-        demo:"https://github.com/Graycouch/Gamehub",
-        github:"https://github.com/Graycouch/Gamehub"
-    },
-    {
-        id:3,
         name:"Web Portfolio",
-        description:"Personal Portfolio that I built from scratch, it showcases all my relevant experiences and projects in the form of an easy to access website that also provides a contact button to easily contact me",
-        tags:["React", "Javascript", "HTML", "CSS"],
+        description:"Built a personal web portfolio from scratch to display professional experience, projects, and technical skills through a clean, responsive design.",
+        tags:["JavaScript", "React.js", "HTML5"],
         
         demo:"https://github.com/Graycouch/Web-Portfolio",
         github:"https://github.com/Graycouch/Web-Portfolio"
         
-    },
+    }
+    {
+        id:3,
+        name:"GameHub",
+        description:"Created a full-stack web application that allows gamers worldwide to connect, form groups, and communicate in real time using the MERN stack (MongoDB, Express.js, React.js, Node.js).",
+        tags:["MongoDB","React.js","Node.js"],
+        
+        demo:"https://github.com/Graycouch/Gamehub",
+        github:"https://github.com/Graycouch/Gamehub"
+    },,
     {
         id:4,
         name:"Gesture Controlled Robotic Hand",
-        description:"Robotic Hand model that mimics the hand movements and gestures of a person through the use of a sensor that sends the information to a servo motor to control the Robotic Hand movements",
-        tags:["Python","Leap Motion","Arduino"],
+        description:"Designed and built a robotic hand system capable of mimicking real-time human gestures through the integration of motion sensors and servo motors.",
+        tags:["Arduino","Python","C"],
         
         demo:"https://github.com/Graycouch/GestureControlledRoboticHand",
         github:"https://github.com/Graycouch/GestureControlledRoboticHand"
