e95f228ac4579b4a5ea35b36a450f906aa00c38d
Abdel Rahman Mansour <abdelrahmanmansour@outlook.com>
2026-05-24 16:30:16 -0500

    Updated Photo For Intro

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -190,7 +190,7 @@ const Intro = () => {
   useEffect(() => {
     init(textRef.current, {
       showCursor: false, 
-      strings: ["Software Engineer", "Research Scholar", "LSU Tiger", "Computer Scientist", "Avid Gamer", "Anime Connoisseur"]
+      strings: ["Hanging Around", "Software Engineer", "Research Scholar", "LSU Tiger", "Computer Scientist", "Avid Gamer", "Anime Connoisseur"]
     });
   }, []);
 
